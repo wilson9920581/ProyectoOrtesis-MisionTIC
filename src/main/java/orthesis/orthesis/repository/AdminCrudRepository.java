cb8646c23c6682185a2898cb25fb6727250b4647
wilson9920581 <wlancheros@gmail.com>
2022-10-13 20:51:57 -0500

    Tercer commit

--- src/main/java/orthesis/orthesis/repository/AdminCrudRepository.java
+++ src/main/java/orthesis/orthesis/repository/AdminCrudRepository.java
@@ -7,6 +7,6 @@ import orthesis.orthesis.models.Admin;
 import java.util.List;
 // el Repository es para hacer operaciones con la base de datos
 public interface AdminCrudRepository extends CrudRepository<Admin, Integer> {
-    @Query("SELECT c.name, COUNT(c.name) FROM Admin AS c group by c.name order by count(c.name) desc")
-    public List<Object[]> countTotalAdminByModel();
+   // @Query("SELECT c.name, COUNT(c.name) FROM Admin AS c group by c.name order by count(c.name) desc")
+    //public List<Object[]> countTotalAdminByModel();
 }
--- src/main/java/orthesis/orthesis/repository/OrtesisCrudRepository.java
+++ src/main/java/orthesis/orthesis/repository/OrtesisCrudRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 //El Repositorio es para hacer operaciones con la base de datos
 public interface OrtesisCrudRepository extends CrudRepository<Ortesis, Integer> {
 
-    @Query("SELECT c.model, COUNT(c.model) FROM Ortesis AS c group by c.model order by count(c.model) desc") //crear consulta
-    public List<Object[]> countTotalOrtesisByModel();
+  //  @Query("SELECT c.model, COUNT(c.model) FROM Ortesis AS c group by c.model order by count(c.model) desc") //crear consulta
+  //  public List<Object[]> countTotalOrtesisByModel();
 
 }
